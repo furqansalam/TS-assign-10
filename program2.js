ea7d5c21d78ed60d07683eac710ff78c3f7fb370
furqansalam <166218116+furqansalam@users.noreply.github.com>
2024-04-26 18:06:39 +0500

    Add files via upload

---
+++ program2.js
@@ -0,0 +1,4 @@
+//exerside 10
+//my name is BABAR
+//dATED : 18/02/2024
+console.log(5 * 2);
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//exerside 10
+//my name is BABAR
+//dATED : 18/02/2024
+//this program will multiplication
+console.log(5*2);
